59752c1324bdeea435a7be7ee43fcd9b82c3abee
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-07-08 08:35:29 +0900

    レイアウト　style scopedにCSSを定義、中央揃えにして、フッタはページ下部にする

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <header>ヘッダー</header>
-  <main class="main">メイン</main>
-  <footer>フッター</footer>
+  <div class="wrap">
+    <header>ヘッダー</header>
+    <main class="main">メイン</main>
+    <footer>フッター</footer>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 90vh;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+}
+
+.main {
+  flex: 1;
+  padding: 16px;
+}
+</style>
