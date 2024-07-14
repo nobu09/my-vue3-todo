f3982a2f22d98277ae9033066c8d9a1ad99a612b
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-07-14 11:42:11 +0900

    Revert "style scopedの実験 scoped外すと他の要素に適用される"
    This reverts commit 112705a24edc2eec0cf51728a80cacbb921d2f74.

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,15 +2,11 @@
   <footer class="footer">&copy; 2024 my-vue3-todo</footer>
 </template>
 
-<style>
+<style scoped>
 .footer {
   width: 100%;
   height: 30px;
   text-align: center;
   box-shadow: 0 1px 1px #ddd;
-}
-
-.title {
-  color: red;
 }
 </style>
