112705a24edc2eec0cf51728a80cacbb921d2f74
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-07-14 11:40:24 +0900

    style scopedの実験 scoped外すと他の要素に適用される

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,11 +2,15 @@
   <footer class="footer">&copy; 2024 my-vue3-todo</footer>
 </template>
 
-<style scoped>
+<style>
 .footer {
   width: 100%;
   height: 30px;
   text-align: center;
   box-shadow: 0 1px 1px #ddd;
+}
+
+.title {
+  color: red;
 }
 </style>
