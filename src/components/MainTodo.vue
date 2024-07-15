c13425de1e5e9eae5280c3a7df7dfb03c734cc4e
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-07-15 12:53:23 +0900

    MainTodoコンポーネントの追加

---
+++ src/components/MainTodo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <input type="text" class=""todo-input" placeholder="+ TODOを入力" />
+    <button class="btn">追加</button>
+  </div>
+</template>
+
+<style scoped>
+.todo-input {
+  width: 250px;
+  padding: 6px 8px;
+  margin-right: 8px;
+  font-size: 18px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+}
+
+.btn:active {
+  top: 1px;
+}
+</style>
