b2ef27d979f15d6f59ca2ff20d96e60581f6e011
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-09-07 09:14:26 +0900

    idを引数でもらう

--- src/composables/useTodoList.ts
+++ src/composables/useTodoList.ts
@@ -1,6 +1,6 @@
 import { ref } from 'vue';
 
-export const useTodoList = () => {
+export const useTodoList = (id: number) => {
   const todoList = ref<{ id: number; text: string }[]>([]);
   const ls = localStorage.todoList;
   todoList.value = ls ? JSON.parse(ls) : [];
