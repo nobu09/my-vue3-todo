672366b4b6fa7a7dd4edd5d1664d11549e97b122
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-09-07 09:13:49 +0900

    todoListをlocalStorageから取得する処理

--- src/composables/useTodoList.ts
+++ src/composables/useTodoList.ts
@@ -1,4 +1,10 @@
+import { ref } from 'vue';
+
 export const useTodoList = () => {
+  const todoList = ref<{ id: number; text: string }[]>([]);
+  const ls = localStorage.todoList;
+  todoList.value = ls ? JSON.parse(ls) : [];
+
   const findTodo = todoList.value.find((todo) => todo.id === id);
   const idx = todoList.value.findIndex((todo) => todo.id === id);
 
