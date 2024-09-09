6c974f4af81b97a6f8e2faa8e5a543ea64e31099
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-09-09 09:09:21 +0900

    todoListに追加する関数をuseTodoListに追加

--- src/composables/useTodoList.ts
+++ src/composables/useTodoList.ts
@@ -5,5 +5,16 @@ export const useTodoList = (id: number) => {
   const ls = localStorage.todoList;
   todoList.value = ls ? JSON.parse(ls) : [];
 
-  return { todoList };
+  const add = (task: string) => {
+    // IDを簡易的にミリ秒で登録する
+    const id = new Date().getTime();
+
+    // 配列に入力TODOを追加
+    todoList.value.push({ id: id, text: task });
+
+    // localStorageに保存
+    localStorage.todoList = JSON.stringify(todoList.value);
+  };
+
+  return { todoList, add };
 };
