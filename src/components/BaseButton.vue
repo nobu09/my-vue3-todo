d3cd059fc27bb321896fc646764d49fc84b6a03a
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-11-24 21:01:38 +0900

    BaseButton.vueを追加

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const aaa = () => {};
+</script>
+
+<template>
+  <button class="btn" @click="aaa">XXX</button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 8px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  background-color: #03a9f4;
+  border-radius: 6px;
+}
+</style>
