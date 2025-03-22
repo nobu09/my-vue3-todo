c1a54dc4c3199d88388e83aa614549ed04cf4863
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-03-22 11:24:16 +0900

    ButtonEditを追加

---
+++ src/components/ButtonEdit.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+
+// type を使わず直接書く方法
+const emit = defineEmits<{ 'edit-click': [void] }>();
+</script>
+
+<template>
+  <!-- // @on-clickにemitを直接書いている -->
+  <BaseButton color="green" @on-click="emit('edit-click')">変更</BaseButton>
+</template>
