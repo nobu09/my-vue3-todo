4a4db24275b807fadf4a79a21421188766bc2a40
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-04-09 08:56:54 +0900

    import MainTodo from '@/views/MainTodo.vue';で読み込めないので色々やってみたがだめ

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import MainTodo from '../views/MainTodo.vue';
+import MainTodo from '@/views/MainTodo.vue';
 
 const router = createRouter({
   history: createWebHistory(),
