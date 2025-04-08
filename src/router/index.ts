502f5dba13e368206cf52bfb9ec09ad62f101967
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-04-09 08:48:11 +0900

    ルートを設定する　src/route/index.tsにroutesの配列にMainTodoを設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import MainTodo from '../views/MainTodo.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'mainTodo',
+      component: MainTodo
+    }
+  ]
 });
 
 export default router;
