ae16aa9d3f83ae4a94f862ae0deb7b520495b188
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-17 06:45:33 +0900

    NotFoundを追加した

---
+++ src/views/NotFound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box_error">
+    <div class="error">
+      <div class="code">
+        <p>404</p>
+      </div>
+      <div class="message_en">
+        <p>Page</p>
+        <p>Not</p>
+        <p>Found</p>
+      </div>
+    </div>
+    <div class="message_ja">お探しのページは見つかりませんでした。</div>
+  </div>
+</template>
+
+<style scoped>
+.box_error {
+  width: 460px;
+  height: 260px;
+  border: 1px solid #aaa;
+}
+
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.code {
+  font-size: 64px;
+  font-weight: bold;
+  color: #888;
+}
+
+.msg_en {
+  margin: 0;
+  font-size: 16px;
+}
+
+.msg_ja {
+  font-size: 18px;
+  text-align: center;
+}
+</style>
