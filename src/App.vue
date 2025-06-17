4646b991650173d8996775b405f3863bc98dfe5f
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-18 07:05:18 +0900

    router-linkでリンクでの遷移

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@ import TheFooter from '@/components/TheFooter.vue';
 <template>
   <div class="wrap">
     <TheHeader />
+    <nav>
+      <router-link to="/">Todo</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
   </div>
