fc379aba235084bf08c2fd628093dc6c04a8de0a
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-20 09:36:43 +0900

    App.vueでメニューにBlogを追加し、@clickを設定する

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import TheHeader from '@/components/TheHeader.vue';
 import TheFooter from '@/components/TheFooter.vue';
+
+const router = useRouter();
+const goBlog = () => {
+  router.push('/blog');
+};
 </script>
 
 <template>
@@ -9,6 +15,7 @@ import TheFooter from '@/components/TheFooter.vue';
     <nav>
       <router-link to="/">Todo</router-link>
       <router-link to="/about">About</router-link>
+      <span @click="goBlog">Blog</span>
     </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
