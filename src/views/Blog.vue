9ca14cdfbca5b19e6527373e047218aafb6f2bfa
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-24 09:07:51 +0900

    パラメータを取得する useRoute()を使う

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-console.log('Blog');
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+// routes で設定した :id と同じ名前を指定する
+const id = ref(route.params.id);
 </script>
 
 <template>
-  <p>blog page</p>
+  <h1>blog page</h1>
+  <p>blog id = {{ id }}</p>
 </template>
