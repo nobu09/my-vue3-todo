c50569fd088059257b3d748b120f831a4094b170
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-26 09:54:53 +0900

    watch APIでrouteの変更を監視する

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 // routes で設定した :id と同じ名前を指定する
 const id = ref(route.params.id);
+
+console.log(`watch外: ${id.value}`);
+
+watch(route, () => {
+  id.value = route.params.id;
+  console.log(`watch内: ${id.value}`);
+});
 </script>
 
 <template>
