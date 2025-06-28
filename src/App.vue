178a1e21f00ff577798d6e7368a8e0d40b0a3c9c
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-06-28 17:03:18 +0900

    Blogメニューを1つだけにする

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,7 @@ const goBlog = () => {
     <TheHeader />
     <nav>
       <router-link to="/">Todo</router-link>
-      | <router-link to="/about">About</router-link> | <span @click="goBlog">Blog</span> |
-      <router-link to="/blog/1">Blog1</router-link> | <router-link to="/blog/2">Blog2</router-link>
+      | <router-link to="/about">About</router-link> | <router-link to="/blog">Blog</router-link> |
     </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
