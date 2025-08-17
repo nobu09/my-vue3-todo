8f88bd8904daa920f2ea6689b463adc4eeec5049
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-08-17 18:57:34 +0900

    ブログ詳細画面の実装

--- src/views/BlogDetail.vue
+++ src/views/BlogDetail.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { PostResponse } from './Blog.vue';
 
 const route = useRoute();
 const id = route.params.id;
+const post = ref<PostResponse>();
+
+const fetchData = async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  post.value = await response.json();
+};
+
+onMounted(fetchData);
 </script>
 
 <template>
-  {{ id }}
+  <div>
+    <h1 class="title">{{ post?.title }}</h1>
+    <div class="detail">{{ post?.body }}</div>
+  </div>
 </template>
+
+<style scoped>
+.title {
+  margin: 12px;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.detail {
+  line-height: 1.5;
+}
+</style>
